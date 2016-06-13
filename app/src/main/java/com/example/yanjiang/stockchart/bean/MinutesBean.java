c0d19645ae51aa0df8f965006a722aeb5186a549
ajiang <102506518@qq.com>
2016-06-13 22:27:50 +0800

    lll

---
+++ app/src/main/java/com/example/yanjiang/stockchart/bean/MinutesBean.java
@@ -0,0 +1,12 @@
+package com.example.yanjiang.stockchart.bean;
+
+/**
+ * Created by Administrator on 2016/6/6.
+ */
+public class MinutesBean {
+    public Float chengjiaojia,junjia,total,change,percentage;
+    public int chengjiaoliang;
+    public String time;
+    public int color;
+
+}
