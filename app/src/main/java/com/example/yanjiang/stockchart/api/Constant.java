e85ea2d7dcb86b0a117e175ab8f2c21b07235871
ajiang <1025065158@qq.com>
2016-06-13 13:39:53 +0800

    优化

--- app/src/main/java/com/example/yanjiang/stockchart/api/Constant.java
+++ app/src/main/java/com/example/yanjiang/stockchart/api/Constant.java
@@ -1,8 +1,8 @@
 package com.example.yanjiang.stockchart.api;
 
 public class Constant {
-
-    public static final String MINUTESURL = "activity中使用离线方法";
-    public static final String DETAILURL="activity中使用离线方法";
+/*注此处为假接口，用离线数据测试即可，如需网络测试，可邮件我获取接口*/
+    public static final String MINUTESURL = "http:/test/";
+    public static final String DETAILURL="test/";
 
 }
