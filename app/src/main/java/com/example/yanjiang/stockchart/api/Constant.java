92acdd6bf54bebadc2103a8cd5e9cca087198144
ajiang <1025065158@qq.com>
2016-06-13 13:48:39 +0800

    ignore

--- app/src/main/java/com/example/yanjiang/stockchart/api/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.example.yanjiang.stockchart.api;
-
-public class Constant {
-/*注此处为假接口，用离线数据测试即可，如需网络测试，可邮件我获取接口*/
-    public static final String MINUTESURL = "http:/test/";
-    public static final String DETAILURL="test/";
-
-}
