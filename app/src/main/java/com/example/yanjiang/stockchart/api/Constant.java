7499d3f79719d42dbc70fd1219dde6907110bafa
ajiang <102506518@qq.com>
2016-06-17 23:33:08 +0800

    st

---
+++ app/src/main/java/com/example/yanjiang/stockchart/api/Constant.java
@@ -0,0 +1,9 @@
+package com.example.yanjiang.stockchart.api;
+
+/**
+ * Created by Administrator on 2016/6/13.
+ */
+public class Constant {
+    public static final String MINUTESURL="http://test/";
+    public static final String DETAILURL="test/";
+}
